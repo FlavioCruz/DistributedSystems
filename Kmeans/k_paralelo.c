e43f68ac2e141d48107ed9d23c2d55a4f4b83495
root <root@localhost.localdomain>
2018-04-26 07:52:51 -0300

    bug aparentemente na linha 155: Segmentation Fault (11)

--- Kmeans/k_paralelo.c
+++ Kmeans/k_paralelo.c
@@ -18,26 +18,25 @@ float calculaDistancia(float pontoX,float pontoY, float centroideX, float centro
 
 int* somaVetores(int* vetor1, int* vetor2){
   int i;
-  printf("\t\tcomeca soma \n");
   for(i=0; i<k; i++)
     vetor1[i]+= vetor2[i];
-
-  printf("\t\t acaba soma\n");
   return vetor1;
 }
 
-float*  somaVetores1(float* vetor1, float* vetor2){
+float* somaVetoresFloat(float* vetor1, float* vetor2){
   int i;
-  for(i=0; i<k; i++)
+  for(i=0; i<k*2; i++)
     vetor1[i]+= vetor2[i];
   return vetor1;
 }
 
 void imprimeCentroides(float* centroides){
     int i;
-    printf("Centroides encontrados:\n");
-    for (i = 0; i <= k; i+=2)
-      printf("%f %f\n", centroides[i], centroides[i+1]);
+    printf("Centroides encontrados, k = %d \n", k);
+    for (i = 0; i < k*2; i++)
+      printf("dentro antes\n");
+      printf("%f \n", centroides[i]);
+      printf("dentro depois\n");
 }
 
 /** Fim funcoes auxiliares*/
@@ -47,14 +46,14 @@ int achaCentroide(float pontoX, float pontoY, float* centroides){
   int i, indice;
   float distanciaAtual = INT_MAX;
   float distancia;
-  for (i = 0; i < k; i+=2) {
+  for (i = 0; i < k*2; i+=2) {
     distancia = calculaDistancia(pontoX, pontoY, centroides[i], centroides[i+1]);
     if(distancia < distanciaAtual){
       distanciaAtual = distancia;
       indice = i;
     }
   }
-  return indice;
+  return indice/2;
 }
 
 //Funcao que obtem os novos centroides
@@ -62,96 +61,109 @@ int achaCentroide(float pontoX, float pontoY, float* centroides){
 //De pontos pertencentes aquele grupo.
 float* calculaCentroide(int* quantidade, float* somas, float* centroides){
   int i;
-  for (i = 0; i < k; i+=2) {
-    centroides[i] = somas[i]/quantidade[i];
-    centroides[i+1] = somas[i+1]/quantidade[i];
+  for (i = 0; i < k*2; i+=2) {
+    centroides[i] = somas[i]/quantidade[i/2];
+    centroides[i+1] = somas[i+1]/quantidade[i/2];
   }
 
   return centroides;
 }
 
 void kmeans(float* pontos, float* centroides, int inicio){
-  int i,j, mudancas, index;
+  int i,j, mudancas, mudancasTemp,index;
   int* totalCentroide = (int*) malloc(sizeof(int)*k);
   int indices[quantidade];
-  float* somaCentroide = (float*) malloc(sizeof(float*) * k*2);
+  float* somaCentroide = (float*)calloc(k*2,sizeof(float));
+  int* totalTemp = (int*)calloc(k,sizeof(int));
+  float* somaTemp = (float*)malloc(k*2*sizeof(float));
   float limite = 0.00001;
   int max = 100;
   MPI_Status status;
-  int iteracoes = 0;
-  int termina = 1;
+  int iteracoes = 10;
+  int termina = 0;
 
-  for(i=0; i<k; i++) {
-    totalCentroide[i] = 0;
-    somaCentroide[i]= 0;
-    somaCentroide[i+1] = 0;
-  }
+  int contador = 0;
 
   do{
+    printf("contador1: %d, rank: %d\n", contador, meu_rank);
+    for(i=0; i<k; i++) {
+      totalCentroide[i] = 0;
+      somaCentroide[i]= 0;
+      somaCentroide[i+1] = 0;
+    }
 
     iteracoes++;
     mudancas = 0;
     printf("meu rank: %d, inicio: %d, quantidade: %d, inicio %d\n", meu_rank, inicio, quantidade, inicio);
-    for(i=inicio; i<=inicio + quantidade; i+=2){
+    for(i=inicio; i<inicio + quantidade*2-1; i+=2){
         index = achaCentroide(pontos[i], pontos[i+1], centroides);
         if(indices[i] != index){
             indices[i] = index;
-            totalCentroide[index]++;
-            somaCentroide[i] += pontos[i];
-            somaCentroide[i+1] += pontos[i+1];
             mudancas++;
         }
-        printf("rank %d indice %d\n", meu_rank, index);
+        somaCentroide[index*2] += pontos[i];
+        somaCentroide[index*2+1] += pontos[i+1];
+        totalCentroide[index]++;
+        // printf("rank %d indice %d\n", meu_rank, index);
     }
 
 //!!!!! Arrumar daqui pra baixo, verificar se send e recv tem & na frente
     if(meu_rank != 0){
-      
- //      MPI_Send(&mudancas, 1, MPI_INT, 0, 1, MPI_COMM_WORLD);
+
+       MPI_Send(&mudancas, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
 
        MPI_Send(totalCentroide, k, MPI_INT, 0, 2, MPI_COMM_WORLD);
-  //     MPI_Send(somaCentroide, k*2, MPI_INT, i, 1, MPI_COMM_WORLD);
+       MPI_Send(somaCentroide, k*2, MPI_FLOAT, 0, 1, MPI_COMM_WORLD);
 
-      // MPI_Recv(&termina, 1, MPI_INT, MPI_ANY_SOURCE, 1, MPI_COMM_WORLD, &status);
+
 
     }else{
-      int* totalTemp = (int*)calloc(k,sizeof(int));
       int j;
 
        for(i=1; i< np; i++){
         MPI_Recv(totalTemp, k, MPI_INT, MPI_ANY_SOURCE, 2, MPI_COMM_WORLD, &status);
+        totalCentroide = somaVetores(totalCentroide, totalTemp);
 
-         totalCentroide = somaVetores(totalCentroide, totalTemp);
+        MPI_Recv(somaTemp, k*2, MPI_FLOAT, MPI_ANY_SOURCE, 1, MPI_COMM_WORLD, &status);
+
+        somaCentroide = somaVetoresFloat(somaCentroide, somaTemp);
+
+          int j;
+          for(j=0; j<k*2; j++) printf("soma %f\n", somaCentroide[j]);
+
+        centroides = calculaCentroide(totalCentroide, somaCentroide, centroides);
+        MPI_Recv(&mudancasTemp, 1, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
+        mudancas+=mudancasTemp;
        }
 
 
-    for(i=0; i< k; i++) printf("total atual %d \n", totalCentroide[i]);
 
 
-        // somaCentroide = somaVetores(somaCentroide, vetTemp);
-        // MPI_Recv(&vetTemp, k, MPI_INT, MPI_ANY_SOURCE, 1, MPI_COMM_WORLD, &status);
-        // totalCentroide = somaVetores(totalCentroide, vetTemp);
-      //    MPI_Recv(&mudancasTemp, 1, MPI_INT, MPI_ANY_SOURCE, 1, MPI_COMM_WORLD, &status);
-      //    mudancas+=mudancasTemp;
-      // }
-
-      //!!MUDAR SEND PARA BROADSCAST QUANDO MEU_RANK = 0
-      if(mudancas/total > limite && iteracoes < max){
-        termina = 0;
-        printf("etste\n");
-        centroides = calculaCentroide(totalCentroide, somaCentroide, centroides);
-        printf("c1 %f %f", centroides[0], centroides[1]);
+      //!!MUDAR SEND PARA BROADCAST QUANDO MEU_RANK = 0
+      if(iteracoes == max && mudancas/total <= limite){
+        termina = 1;
       }
 
-      printf("termina: %d\n", termina);
-  //    MPI_Bcast(&termina, 1, MPI_INT, 0, MPI_COMM_WORLD);
-
-   //   MPI_Bcast(&centroides, k, MPI_FLOAT, 0, MPI_COMM_WORLD);
-
     }
-  } while (0);
+    printf("aaaaaaaaa\n");
+    printf("termina: %d, rank: %d\n", termina, meu_rank);
+    MPI_Bcast(&termina, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
- // imprimeCentroides(centroides);
+    printf("termina: %d\n", termina);
+    printf("aqui, meu rank: %d\n", meu_rank);
+    imprimeCentroides(centroides);
+    MPI_Bcast(centroides, k, MPI_FLOAT, 0, MPI_COMM_WORLD);
+    printf("abc\n");
+    printf("contador: %d, rank: %d\n", contador, meu_rank);
+    contador++;
+  } while (!termina);
+  printf("acabou1");
+  free(totalCentroide);
+  free(somaCentroide);
+  free(totalTemp);
+  free(somaTemp);
+  printf("acabou");
+  //imprimeCentroides(centroides);
 }
 
 
@@ -162,7 +174,7 @@ int main(int argc, char** argv){
   MPI_Comm_rank(MPI_COMM_WORLD, &meu_rank);
   MPI_Comm_size(MPI_COMM_WORLD,&np);
 
-  //Se for processo master, lê o ar]quivo para então distribuir os pontos entre os processos
+  //Se for processo master, lê o arquivo para então distribuir os pontos entre os processos
 
   char nomeArquivo[255];
   total = 0;
@@ -185,12 +197,12 @@ int main(int argc, char** argv){
     fscanf(arquivo, "%d", &total);
   }
 
-    MPI_Bcast(&total, 1, MPI_INT, 0, MPI_COMM_WORLD); 
+    MPI_Bcast(&total, 1, MPI_INT, 0, MPI_COMM_WORLD);
     MPI_Bcast(&k, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
     float *centro = (float*) malloc(k * 2 * sizeof(float));
     float *pontos = (float*) malloc(total * 2 * sizeof(float));
-    
+
     if(meu_rank == 0){
       int i;
       for(i = 0; i < total * 2; i++){
@@ -205,8 +217,8 @@ int main(int argc, char** argv){
       fclose(arquivo);
   }
 
-    
-  MPI_Bcast(pontos, total*2, MPI_FLOAT, 0, MPI_COMM_WORLD);    
+
+  MPI_Bcast(pontos, total*2, MPI_FLOAT, 0, MPI_COMM_WORLD);
   MPI_Bcast(centro, k*2, MPI_FLOAT, 0, MPI_COMM_WORLD);
 
 
@@ -222,13 +234,15 @@ int main(int argc, char** argv){
   } else {
     inicio = quantidade * meu_rank *2 + resto*2;
   }
-  
-  
+
+
 
   //Chama o procedimento
   kmeans(pontos, centro, inicio);
 
 
   printf("done\n");
+  free(pontos);
+  free(centro);
   MPI_Finalize();
 }
